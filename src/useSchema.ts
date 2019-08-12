571721b9577258eeb253337e17082c559205224f
Artem Zakharchenko <kettanaito@gmail.com>
2019-08-12 16:34:46 +0200

    Adds support for the "optional" function

--- src/useSchema.ts
+++ src/useSchema.ts
@@ -1,6 +1,10 @@
+import getNestedProperty from './utils/getNestedProperty'
+
 interface Schema {
-  [key: string]: ResolversGroup | Resolver | Schema
+  [key: string]: SchemaValue
 }
+
+type SchemaValue = ResolversGroup | Resolver | Schema
 
 type Pointer = string[]
 
@@ -41,33 +45,45 @@ function getErrorsBySchema(schema: Schema, data: Object, pointer: Pointer) {
     const currentPointer = pointer.concat(key)
 
     // Handle values that are expected by schema, but not defined in data.
-    if (typeof data === 'undefined' || data === null) {
+    if (data == null) {
       return errors.concat(createValidationError(currentPointer, data))
     }
 
     const value = data[key]
-    const resolver = schema[key]
+    const resolverPayload = schema[key]
+
+    // When a resolver function returns an Array,
+    // treat the first argument as a predicate that determines if validation is necessary.
+    // Threat the second argument as the resolver function.
+    const [predicate, resolver] = Array.isArray(resolverPayload)
+      ? resolverPayload
+      : [() => true, resolverPayload]
+    const shouldValidate = predicate(value, key, currentPointer, data, schema)
+
+    if (!shouldValidate) {
+      return errors
+    }
 
     if (typeof resolver === 'object') {
       return errors.concat(getErrorsBySchema(resolver, value, currentPointer))
     }
 
-    const resolverOutput = resolver(value, currentPointer)
+    const resolverVerdict = resolver(value, currentPointer)
 
     // If resolver returns an Object, treat it as named rules map.
-    if (typeof resolverOutput === 'object') {
-      const namedErrors = Object.keys(resolverOutput)
-        .filter((rule) => !resolverOutput[rule])
+    if (typeof resolverVerdict === 'object') {
+      const namedErrors = Object.keys(resolverVerdict)
+        // Note that named resolvers keep a boolean value,
+        // not the verdict function. Therefore, no calls.
+        .filter((rule) => !resolverVerdict[rule])
         .reduce((acc, rule) => {
-          return acc.concat(
-            createValidationError(currentPointer, value, rule),
-          )
+          return acc.concat(createValidationError(currentPointer, value, rule))
         }, [])
       return errors.concat(namedErrors)
     }
 
-    // Otherwise resolver is expected to return a boolean.
-    return resolverOutput
+    // Otherwise resolver is a function that returns a boolean verdict
+    return resolverVerdict
       ? errors
       : errors.concat(createValidationError(currentPointer, value))
   }, [])
@@ -86,6 +102,19 @@ function createValidationError(
     status,
     rule,
   }
+}
+
+/**
+ * High-order resolver that applies a given resolver function
+ * only when the associated property is present in the actual data.
+ */
+export const optional = (payload: SchemaValue) => {
+  return [
+    (value, key, pointer, data, schema) => {
+      return getNestedProperty(pointer, data) != null
+    },
+    payload,
+  ]
 }
 
 export default useSchema
